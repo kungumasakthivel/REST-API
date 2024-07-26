cd0754531bf1e13fe5014790a1d155202c63dcca
kungumasakthivel <kungumasakthivel.k@gmail.com>
2024-07-26 10:51:59 +0000

    commit:

---
+++ src/main/java/com/example/goodreads/BookController.java
@@ -0,0 +1,9 @@
+package com.example.goodreads;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+    @GetMapping
+}
